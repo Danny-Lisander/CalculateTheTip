e84be73ca67072a101e550731eb32b74773340e4
Danny <81091123+Danny-Lisander@users.noreply.github.com>
2022-12-20 08:42:08 +0600

    test

--- app/src/main/java/com/dany/calculateTheTip/MainActivity.kt
+++ app/src/main/java/com/dany/calculateTheTip/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.dany.calculateTheTip
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.dany.calculateTheTip.databinding.ActivityMainBinding
 import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
 
